3fc4b6f18a72b06cdba8838a75931cf83f34f42f
Huy Duong <qhuyduong@hotmail.com>
2023-07-24 00:17:30 +0700

    Add some protocols
    Signed-off-by: Huy Duong <qhuyduong@hotmail.com>

--- types/efi.h
+++ types/efi.h
@@ -5,5 +5,10 @@
 #include "UefiSpec.h"
 #include "Protocol/Pcd.h"
 #include "Protocol/MmBase.h"
+#include "Protocol/LoadedImage.h"
+#include "Protocol/PiPcd.h"
+#include "Protocol/PcdInfo.h"
+#include "Protocol/PiPcdInfo.h"
+#include "Protocol/MmAccess.h"
 
 #endif
