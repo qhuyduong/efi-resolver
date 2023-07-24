9fde6cdb6d45f25f1bfc8c02fabbc501a323a757
Huy Duong <qhuyduong@hotmail.com>
2023-07-24 10:43:32 +0700

    Rearrange efi.h
    Signed-off-by: Huy Duong <qhuyduong@hotmail.com>

--- types/efi.h
+++ types/efi.h
@@ -3,18 +3,24 @@
 
 #include "Uefi/UefiBaseType.h"
 #include "Uefi/UefiSpec.h"
-#include "Protocol/Pcd.h"
-#include "Protocol/MmBase.h"
-#include "Protocol/LoadedImage.h"
-#include "Protocol/PiPcd.h"
-#include "Protocol/PcdInfo.h"
-#include "Protocol/PiPcdInfo.h"
-#include "Protocol/MmAccess.h"
+
 #include "Protocol/DevicePathUtilities.h"
-#include "Protocol/SmbusHc.h"
-#include "Protocol/HiiString.h"
-#include "Protocol/HiiDatabase.h"
+
 #include "Protocol/HiiConfigRouting.h"
+#include "Protocol/HiiDatabase.h"
+#include "Protocol/HiiString.h"
+
+#include "Protocol/LoadedImage.h"
+
+#include "Protocol/MmBase.h"
+#include "Protocol/MmAccess.h"
 #include "Protocol/MmCommunication.h"
+
+#include "Protocol/Pcd.h"
+#include "Protocol/PcdInfo.h"
+#include "Protocol/PiPcd.h"
+#include "Protocol/PiPcdInfo.h"
+
+#include "Protocol/SmbusHc.h"
 
 #endif
