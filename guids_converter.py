ec013e916f9a47d5a79bc339def70ae07dbef03f
Huy Duong <qhuyduong@hotmail.com>
2023-07-25 12:02:12 +0700

    Update efi.c
    Signed-off-by: Huy Duong <qhuyduong@hotmail.com>

--- guids_converter.py
+++ guids_converter.py
@@ -1,17 +1,15 @@
 import re
 import sys
 
-
 def convert_file(input_file, output_file):
     with open(input_file, "r") as infile, open(output_file, "w") as outfile:
         for line in infile.readlines():
             name = line.split("=")[0].replace(" ", "")
             guid = line.split("=")[1].replace(" ", "")
             outfile.write(f"///@protocol {guid}")
-            outfile.write(f"///@binding {name} {guid}")
             protocol = re.sub(r"(?<!^)(?=[A-Z])", "_", name[1:]).upper()
+            outfile.write(f"///@binding {protocol} {guid}")
             outfile.write(f"struct {protocol.replace('_GUID', '')}\n\n")
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
